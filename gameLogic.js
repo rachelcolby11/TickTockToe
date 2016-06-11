e5846eb18d1a9d77f72a8ddbe0d4c419d369e91d
Rachel Colby <rachelcolby11@gmail.com>
2016-06-10 23:06:58 -0700

    Add beginnings of game logic to respond to players' moves

--- gameLogic.js
+++ gameLogic.js
@@ -1,1 +1,15 @@
 // Connects the user's actions with the logic in boardLogic and checks for a win
+
+$(document).ready(function(){
+	var boardObject = window.Board;
+	var Board = window.Board.board;
+	var TurnCount = window.Board.turn;
+	var squares = document.getElementsByClassName('square');
+
+	$('.square').on('click', function(e){
+		var coordinates = this.attributes.id.nodeValue;
+		var squareObject = boardLogic.findSquare(Board, coordinates);
+		var currentState = squareObject.state;
+		
+	});
+});
