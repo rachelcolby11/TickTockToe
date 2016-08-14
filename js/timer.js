8c870bb9d4504499d4b726f3bd8cb9cef252bac1
Rachel Colby <rachelcolby11@gmail.com>
2016-08-14 00:20:09 -0700

    Prevent multiple countdowns running simultaneously
    With this commit, the countdown will only start if there is no countdown currently running. Before, pressing the Start button when the countdown was already running would start a second countdown without canceling the first, meaning the timer would decrement twice as fast.

--- js/timer.js
+++ js/timer.js
@@ -2,6 +2,9 @@
 
 var timeLeft = 5;
 
+// counting variable tells us if the countdown is currently running
+var counting = false;
+
 // Use the default blue background color if more than 3 seconds left. Set background color to yellow at 3 seconds remaining and red at one second remaining
 var setColor = function() {
   if (timeLeft > 3) {
@@ -28,15 +31,21 @@ var timer = function() {
 var countdown;
 
 var startTimer = function() {
-  countdown = window.setInterval(timer, 1000);
+  // Begin countdown only if countdown is not already running
+  if (!counting) {
+    countdown = window.setInterval(timer, 1000);
+    counting = true;
+  }
 }
 
 var stopTimer = function() {
   clearInterval(countdown);
+  counting = false;
 }
 
 var resetTimer = function() {
   clearInterval(countdown);
+  counting = false;
   timeLeft = 5;
   $('#timer').html(timeLeft);
 }
